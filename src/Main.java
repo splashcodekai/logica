d67211cfaa86614758c7ddb8d2b04e67b9e2d4fb
black cat <kaikoliverlima@gmail.com>
2024-01-01 10:20:46 -0300

    mod11leg

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    listado();
+
+    public static void main(String args[]) {
+        Scanner txt = new Scanner(System.in);
+        String nm;
+        nm = txt.nextLine();
+    }
+
+
+   private static void listado{
+        List<String>lista= new ArrayLista();
+
+    }
+}
